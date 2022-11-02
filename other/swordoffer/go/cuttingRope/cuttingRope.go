1971d8011e011165c77970a755a6d7b4bec46a69
D <d@Ds-MacBook-Pro.local>
2022-11-02 21:21:56 +0800

    add cuttingRope logic

--- other/swordoffer/go/cuttingRope/cuttingRope.go
+++ other/swordoffer/go/cuttingRope/cuttingRope.go
@@ -1,5 +1,20 @@
 package cuttingrope
 
-func cuttingRope() {
+func cuttingRope(n int) int {
+	dp := make([]int, n+1)
+	for i := 2; i <= n; i++ {
+		curMax := 0
+		for j := 1; j < i; j++ {
+			curMax = max(curMax, max(j*(i-j), j*dp[i-j]))
+		}
+		dp[i] = curMax
+	}
+	return dp[n]
+}
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
--- other/swordoffer/go/cuttingRope/cuttingRope_test.go
+++ other/swordoffer/go/cuttingRope/cuttingRope_test.go
@@ -1,1 +1,11 @@
 package cuttingrope
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCuttingRope(t *testing.T) {
+	assert.Equal(t, 36, cuttingRope(10))
+}
