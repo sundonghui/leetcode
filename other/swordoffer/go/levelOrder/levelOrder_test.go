e12162d4dc01e665886b0f72a286e44588a4d964
D <d@Ds-MacBook-Pro.local>
2022-11-22 23:21:39 +0800

    add levelOrder3

--- other/swordoffer/go/levelOrder/levelOrder_test.go
+++ other/swordoffer/go/levelOrder/levelOrder_test.go
@@ -68,3 +68,27 @@ func TestLevelOrder2(t *testing.T) {
 	}
 	assert.Equal(t, expected, levelOrder2(root))
 }
+
+func TestLevelOrder3(t *testing.T) {
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	expected := [][]int{
+		{3},
+		{20, 9},
+		{15, 7},
+	}
+	assert.Equal(t, expected, levelOrder3(root))
+}
