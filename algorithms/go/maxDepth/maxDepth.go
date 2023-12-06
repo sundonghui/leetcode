6228fca4739ef6ccb182ab334f1e6ed26ec8ec63
sundonghui <sundonghui@powerlaw.ai>
2023-12-06 19:14:16 +0800

    [add] maxDepth

---
+++ algorithms/go/maxDepth/maxDepth.go
@@ -0,0 +1,21 @@
+package maxdepth
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
