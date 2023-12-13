118079a5efdce3193347452df3901d2dd385e91c
sundonghui <sundonghui@powerlaw.ai>
2023-12-13 10:18:38 +0800

    [add] buildTree

--- algorithms/go/buildTree/buildTree_test.go
+++ algorithms/go/buildTree/buildTree_test.go
@@ -25,5 +25,25 @@ func Test_buildTree(t *testing.T) {
 		},
 	}
 	assert.Equal(t, &expected, BuildTree(inorder, postorder))
+}
 
+func Test_buildTree_2(t *testing.T) {
+	preorder := []int{3, 9, 20, 15, 7}
+	inorder := []int{9, 3, 15, 20, 7}
+	expected := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	assert.Equal(t, expected, buildTree_2(preorder, inorder))
 }
