1989be90f93f2a566fca2b30aa73f7c56f2e7ef1
sundonghui <sundonghui@powerlaw.ai>
2024-02-18 12:01:57 +0800

    [add] fullJustify

--- algorithms/go/isPalindrome/isPalindrome.go
+++ algorithms/go/isPalindrome/isPalindrome.go
@@ -1,5 +1,7 @@
 package ispalindrome
 
+import "strings"
+
 func isPalindrome(x int) bool {
 	if x < 0 {
 		return false
@@ -19,4 +21,28 @@ func isPalindrome(x int) bool {
 	}
 	r = r*10 + x
 	return r == origin
+}
+
+func IsPalindrome(s string) bool {
+	var sgood string
+	for i := 0; i < len(s); i++ {
+		if isalnum(s[i]) {
+			sgood += string(s[i])
+		}
+	}
+
+	n := len(sgood)
+	sgood = strings.ToLower(sgood)
+	for i := 0; i < n/2; i++ {
+		if sgood[i] != sgood[n-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func isalnum(ch byte) bool {
+	return (ch >= 'A' && ch <= 'Z') ||
+		(ch >= 'a' && ch <= 'z') ||
+		(ch >= '0' && ch <= '9')
 }
--- algorithms/go/strStr/strStr.go
+++ algorithms/go/strStr/strStr.go
@@ -93,3 +93,17 @@ func strStr1(haystack string, needle string) int {
 	}
 	return -1
 }
+
+func strStr2024(haystack string, needle string) int {
+	n, m := len(haystack), len(needle)
+outer:
+	for i := 0; i+m <= n; i++ {
+		for j := range needle {
+			if haystack[i+j] != needle[j] {
+				continue outer
+			}
+		}
+		return i
+	}
+	return -1
+}
