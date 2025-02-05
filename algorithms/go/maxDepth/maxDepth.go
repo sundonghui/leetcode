944f631a0d6e85c549b2d3db6aa16dd12d1186c0
sundonghui <sundonghui@powerlaw.ai>
2025-02-05 22:19:23 +0800

    [modified] maxDepth

--- algorithms/go/maxDepth/maxDepth.go
+++ algorithms/go/maxDepth/maxDepth.go
@@ -6,6 +6,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+/*
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
@@ -18,4 +19,30 @@ func max(i, j int) int {
 		return i
 	}
 	return j
+}
+*/
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{}
+	queue = append(queue, root)
+	ans := 0
+	for len(queue) > 0 {
+		sz := len(queue)
+		for sz > 0 {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			sz--
+		}
+		ans++
+	}
+	return ans
 }
--- algorithms/go/maxDepth/maxDepth_test.go
+++ algorithms/go/maxDepth/maxDepth_test.go
@@ -1,1 +1,43 @@
 package maxdepth
+
+import "testing"
+
+func Test_maxDepth_example1(t *testing.T) {
+	// Input: root = [3,9,20,null,null,15,7]
+	// Output: 3
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	expected := 3
+	if got := maxDepth(root); got != expected {
+		t.Errorf("maxDepth() = %v, want %v", got, expected)
+	}
+}
+
+func Test_maxDepth_example2(t *testing.T) {
+	// Input: root = [1,null,2]
+	// Output: 2
+	root := &TreeNode{
+		Val: 1,
+		Right: &TreeNode{
+			Val: 2,
+		},
+	}
+	expected := 2
+	if got := maxDepth(root); got != expected {
+
+		t.Errorf("maxDepth() = %v, want %v", got, expected)
+	}
+}
