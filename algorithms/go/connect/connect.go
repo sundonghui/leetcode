d9474355b05c1d14dd471a30929cf162d80d55dd
sundonghui <sundonghui@powerlaw.ai>
2025-02-09 22:00:04 +0800

    [add] 117 connect

--- algorithms/go/connect/connect.go
+++ algorithms/go/connect/connect.go
@@ -11,19 +11,19 @@ func connect(root *Node) *Node {
 	if root == nil {
 		return root
 	}
-	queue := []*Node{root}
-	for len(queue) > 0 {
-		tmp := queue
-		queue = nil
-		for i, node := range tmp {
-			if i+1 < len(tmp) {
-				node.Next = tmp[i+1]
+	q := []*Node{root}
+	for len(q) > 0 {
+		temp := q
+		q = nil
+		for i, node := range temp {
+			if i+1 < len(temp) {
+				temp[i].Next = temp[i+1]
 			}
 			if node.Left != nil {
-				queue = append(queue, node.Left)
+				q = append(q, node.Left)
 			}
 			if node.Right != nil {
-				queue = append(queue, node.Right)
+				q = append(q, node.Right)
 			}
 		}
 	}
